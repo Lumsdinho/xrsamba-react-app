82fa1a16908afb8b49abc66f61a3e51cd3f01d11
Lumsden Ruaidhri (HERE) <ruaidhri@lumsden.digital>
2021-06-14 15:51:39 +0100

    setup client and rendered title and description; content needs rendering as md

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import tuneDataImport from './tuneData'
 import Header from './components/Header'
 import Text from './components/Text'
 import Tunesheet from './components/Tunesheet'
 import AudioElements from './components/AudioElements'
+import { client } from './config/contentfulClient'
 
 const App = () => {
 
   const [tuneData] = useState(tuneDataImport)
   const [pageIndex, setPageIndex] = useState(0)
+  const [repertoire, setRepertoire] = useState({})
 
   const handleClick = (index) => {
     setPageIndex(index)
   }
+
+  useEffect(() => {
+    client.getEntries()
+      .then(entries => {
+        setRepertoire(entries.items[0].fields)
+      })
+  })
+
+  // client.getEntries().then(entries => {
+  //   entries.items.forEach(entry => {
+  //     if(entry.fields) {
+  //       console.log(entry.fields)
+  //     }
+  //   })
+  // })
 
   return (      
     <div className="container">
@@ -29,7 +46,7 @@ const App = () => {
       />
 
       <AudioElements  tuneData={tuneData}
-                      pageIndex={pageIndex}/>1
+                      pageIndex={pageIndex}/>
 
       <div className="container" style={{
         backgroundColor:"white",
@@ -37,71 +54,9 @@ const App = () => {
         borderRadius:"2%",
         marginBottom:".5rem"
       }}>
-        <h2 style={{
-          textAlign:"center"
-        }}>Tunes/Breaks</h2>
-        <p><u>Section 1 - Our Main Focus, songs that we should have totally drilled by Aug XR rebellion</u></p>
-        <p><strong>Funk</strong></p>
-          <ul>
-            <li>Break 1</li>
-          </ul>          
-        <p><strong>Karla</strong></p>
-          <ul>
-            <li>Break 1 (gun break)</li>
-            <li>Half gun drum roll</li>
-            <li>Break 2</li>
-            <li>Break 2 inverted</li>
-          </ul>
-        <p><strong>Hedgehog</strong></p>
-          <ul>
-            <li>Break 1</li>
-          </ul>
-        <p><strong>Samba Reggae</strong></p>
-          <ul>
-            <li>Sheffield intro</li>
-            <li>Bra break</li>
-            <li>Break 1</li>
-            <li><em>Break 2</em></li>
-            <li><em>Break 3</em></li>
-          </ul> 
-        <p><strong>Misc</strong></p>
-          <ul>
-            <li>4 beat</li>
-            <li>8 beat</li>
-            <li>4 Rest +/- One instrument plays on</li>
-            <li>8 Rest +/- One instrument plays on</li>
-            <li>Bra Break - N.B. ‘call’ needs to be standardised - we all need to look at ROR and follow that please</li>
-            <li>Knock on the Door Break - N.B. make sure this is done once or multiple times depending on what you signal</li>
-            <li>Bunny break</li>
-            <li>Single out instruments</li>
-            <li>Single instrument drum rolls</li>
-            <li>Snares playing tune on rims</li>
-            <li>XR break</li>
-            <li>Time is running out break</li>
-            <li>Clave break</li>
-            <li>Coffee and cookies break</li>
-          </ul>
-        <p><u>Section 2 - In addition these would be great:</u></p>
-        <p><strong>Afoxe</strong></p>
-          <ul>
-            <li>Break 1</li>
-            <li>Bra break</li>
-            <li>Tamborim stroke</li>
-          </ul> 
-        <p><strong>Drum & Bass</strong></p>
-          <ul>
-            <li>Break 1 (dance break)</li>
-            <li>Break 2</li>
-            <li>Break 3</li>
-            <li>Hip-hop break</li>
-          </ul>
-        <p><strong>Misc</strong></p>
-          <ul>
-            <li>Eurotrash break</li>
-            <li>Scottish tune</li>
-            <li>Crazy Monkey</li>
-            <li>ER break 1 & 2</li>
-          </ul>
+        <h1>{repertoire.title}</h1>
+        <h4>{repertoire.description}</h4>
+        <p>{repertoire.content}</p>        
       </div>                
     </div>
   )
