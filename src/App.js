6de75da49a526e71d841b5391ebef711a43dc98a
Ruaidhri Lumsden <roarbot@pop-os.localdomain>
2021-07-18 21:50:26 +0100

    Moved Header into App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import tuneDataImport from './tuneData'
+import Header from './components/Header'
 import TunePage from './pages/TunePage'
 import Repertoire from './components/Repertoire'
-
 
 const App = () => {
 
   const [tuneData] = useState(tuneDataImport)
   const [pageIndex, setPageIndex] = useState(0)
 
-  const handleClick = (index) => {
-    setPageIndex(index)
-  } 
-
   return (            
     <div className="container">
+      <Header 
+        header={tuneData[pageIndex].title}
+        data={tuneData}
+        setPageIndex={setPageIndex}
+      />    
       <TunePage 
         tuneData={tuneData}
         pageIndex={pageIndex}
--- src/pages/TunePage.js
+++ src/pages/TunePage.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import Header from '../components/Header'
 import Text from '../components/Text'
 import Tunesheet from '../components/Tunesheet'
 import AudioElements from '../components/AudioElements'
-
 
 const TunePage = ({ tuneData, pageIndex, setPageIndex }) => {
   
   return (  
   <>
-    <Header 
-      header={tuneData[pageIndex].title}
-      data={tuneData}
-      setPageIndex={setPageIndex}
-    />
     <Text text={tuneData[pageIndex].text}
     />
     <Tunesheet  src={tuneData[pageIndex].diagram}
